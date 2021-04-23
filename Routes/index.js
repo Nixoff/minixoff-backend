6d227e71041d0c2e946bbcc5bdc952302ce73e53
nixoff <gabadev48@protonmail.com>
2021-04-22 22:56:50 -0300

    :tada: refatorei umas coisas ai

--- Routes/index.js
+++ Routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (rez, res) => {
-    return res.send({message: 'Tudo ok com o método GET da raiz!'});
+    return res.send({message: 'Essa informação é muito importante. Usuários não autorizados não deveriam recebê-las!'});
 })
 
 router.post('/', (rez, res) => {
