da49349f70b6a004bb75b654ba360c38af589c19
nixoff <gabadev48@protonmail.com>
2021-04-22 21:49:42 -0300

    :tada: autenticaçao de usuarios e cript de senha batendo banco

--- Routes/users.js
+++ Routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     Users.find({}, (err, data) => {
         if (err) return res.send({ error: 'Erro na consulta de usuários!' });
         return res.send(data);
     });
-})
+});
 
 router.post('/create', (req, res) => {
     const { name, username, email, password } = req.body;
@@ -25,6 +26,24 @@ router.post('/create', (req, res) => {
             return res.send(data);
         });
     });
+});
+
+router.post('/auth', (req, res) => {
+    const { username,  password } = req.body;
+
+    if(!username || !password) return res.send({ error: 'Dados insuficientes!' });
+
+    Users.findOne({username}, (err, data) => {
+        if(err) return res.send({ error: 'Erro ao buscar usuário!' });
+        if(!data) return res.send({ error: 'Usuário não registrado!' });
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.send ({ error: 'Erro ao autenticar usuário!' });
+
+            data.password = undefined;
+            return res.send(data);
+        })
+    }).select('+password');
 });
 
 module.exports = router;
